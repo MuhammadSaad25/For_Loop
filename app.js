9d9883427ec5c670cf7a46727e3cb646180e30d1
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-08-17 22:17:28 +0500

    Complete

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-
 function printTable() {
     let number;
     let result = "";
     let number1;
-
     number = Number(document.getElementById("number").value);
     number1 = Number(document.getElementById("number1").value);
     for (let i = 1; i <= number1; i++) {
 
         result = (`${result}    <p>    ${number}   x   ${i}   =   ${number * i}    </p>`);
     }
-
+    
     document.getElementById("result").innerHTML = result;
+    let resultDiv = document.querySelector(`#result`)
+    resultDiv.style.display = "flex";
 }
